2313f0dec2ae525a945a9348d8879f54a1015882
Aleksandar Gosevski <gosevskibg@gmail.com>
2015-04-11 20:00:30 +0200

    Remove some files

--- image-loader.js
+++ image-loader.js
@@ -1,32 +1,28 @@
 /*
  * image-loader
  * https://github.com/goschevski/image-loader
- *
- * Copyright (c) 2014 Aleksandar Gosevski
- * Licensed under the MIT license.
+ * Copyright (c) Aleksandar Gosevski
  */
 
 ;(function () {
     var defaults = function (defaults, options) {
-        var obj = {};
-
-        for ( var key in defaults ) {
-            obj[key] = defaults[key];
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                defaults[key] = options[key];
+            }
         }
 
-        for ( var key2 in options ) {
-            obj[key2] = options[key2];
-        }
-
-        return obj;
+        return defaults;
     };
 
-    ImageLoader = function (element, options) {
-        if ( !this.isSupported ) { throw new Error('Older browsers are not supported.'); }
+    var ImageLoader = function (element, options) {
+        if (!this.isSupported) {
+            throw new Error('Older browsers are not supported.');
+        }
 
         var opts = typeof options !== 'undefined' ? options : {};
 
-        if ( typeof opts !== 'object' || Array.isArray(opts) ) {
+        if (typeof opts !== 'object' || Array.isArray(opts)) {
             throw new Error('Options must be passed as object!');
         }
 
@@ -49,13 +45,13 @@
         },
 
         setElements: function (element, options) {
-            this.options = defaults( this.defaults, options );
-            this.$element = document.querySelectorAll( element )[0];
-            this.$placeholder = document.querySelectorAll( this.options.placeholder )[0];
+            this.options = defaults(this.defaults, options);
+            this.$element = document.querySelectorAll(element)[0];
+            this.$placeholder = document.querySelectorAll(this.options.placeholder)[0];
         },
 
         bindEvents: function () {
-            this.$element.addEventListener( 'change', this.render.bind(this), false );
+            this.$element.addEventListener('change', this.render.bind(this), false);
         },
 
         render: function (e) {
@@ -68,10 +64,10 @@
                 return self[ o.method === 'css' ? 'setBackground' : 'setImage' ].bind(self);
             }(uploader.files[0]));
 
-            reader.readAsDataURL( uploader.files[0] );
+            reader.readAsDataURL(uploader.files[0]);
 
             // If callback is function, execute it
-            if ( typeof o.callback === 'function' ) {
+            if (typeof o.callback === 'function') {
                 o.callback();
             }
         },
@@ -79,14 +75,14 @@
         setImage: function (e) {
             var o = this.options;
 
-            var image =  '<img src="'+ e.target.result +'" width="'+ o.width +'" height="'+ o.height +'" class="'+ o.imgClass +'">';
+            var image =  '<img src="' + e.target.result + '" width="' + o.width + '" height="' + o.height + '" class="' + o.imgClass + '">';
 
-            if ( o.method === 'html' ) {
+            if (o.method === 'html') {
                 this.$placeholder.innerHTML = image;
                 return;
             }
 
-            if ( o.method === 'append' ) {
+            if (o.method === 'append') {
                 this.$placeholder.innerHTML = this.$placeholder.innerHTML + image;
                 return;
             }
@@ -101,5 +97,4 @@
     };
 
     window.ImageLoader = ImageLoader;
-
 })();
